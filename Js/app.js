d473713511a2311c5c449704c07ec519007ea60e
Aleksandras Salnik <39255282+aleksxx9@users.noreply.github.com>
2020-06-05 15:13:05 +0300

    Fixes
    fixed natural cash_out error
    removed fetch api in test
    rewritten logic in answer to use Math.ceil()
    Natural function now returns only numbers
    fixed style to match airBnb

--- Js/app.js
+++ Js/app.js
@@ -1,161 +1,159 @@
-const fetch = require("node-fetch");
-const fs = require("fs");
+const fetch = require('node-fetch');
+const fs = require('fs');
 
-fs.readFile(
-  process.argv[2] ? process.argv[2] : "input.json",
-  async function Read (err, json) {
-    //Fetching operation values from Api's
-    if (err) throw err;
-    let data = JSON.parse(json);
-    cashIn = await fetchData(
-      "http://private-38e18c-uzduotis.apiary-mock.com/config/cash-in"
-    );
-    natural = await fetchData(
-      "http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural"
-    );
-    juridical = await fetchData(
-      "http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/juridical"
-    );
-
-    //Getting different id's from given data to count natural limits for each natural user
-    let idsLimit = [];
-    idsLimit = getDifferentId(data);
-    
-    let limit = {};
-
-    //making initial used limit for each user 0
-    idsLimit.map((el) => {
-      limit[el] = 0;
+async function FetchData(url) {
+  const data = fetch(url)
+    .then((json) => json.json())
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
     });
-
-    //Making operations
-    let answer = '';
-    data.forEach((el, i) => {
-      if (el.type == "cash_in") {
-        answer = CashIn(el, cashIn);
-      } else if (el.type == "cash_out") {
-        if (el.user_type == "natural") {
-          limits = Natural(data, i, natural, limit);
-          answer = limits;
-        } else if (el.user_type == "juridical") {
-          answer = Juridical(el.operation.amount, juridical);
-        }
-      }
-      PrintAnswer(answer);
-    });
-  }
-);
-
-async function fetchData(url) {
-  let res = await fetch(url);
-  let data = await res.json();
   return data;
 }
 
 function CashIn(data, cashIn) {
   const percent = (data.operation.amount * cashIn.percents) / 100;
-  if (percent > cashIn.max.amount) return(cashIn.max.amount);
-  else return(percent);
+  if (percent > cashIn.max.amount) return cashIn.max.amount;
+  return percent;
 }
 
-function Natural(data, i, natural, idsLimit) {
-  operationLimit = SameWeek(data, i, idsLimit);
-  idsLimit[data[i].user_id] += data[i].operation.amount;
-  amount = idsLimit[data[i].user_id];
-
-  if (amount <= natural.week_limit) {
-    return('0.00');
+function GetLastOperationDate(data, id, i) {
+  for (let index = i - 1; index >= 0; index -= 1) {
+    if (data[index].user_type === id && data[index].type === 'cash_out') {
+      return data[index].date;
+    } if (index === 0) return data[i].date;
   }
-  else {
-    let result;
-    if (amount > natural.week_limit.amount)
-    {
-      if (data[i].operation.amount == amount) result = (data[i].operation.amount - natural.week_limit.amount) * natural.percents / 100;
-      else result = data[i].operation.amount * natural.percents / 100;
-    }
-    else {
-      result = 0.00;
-    }
-    return(result);
-  }
-}
-
-function Juridical(data, juridical) {
-  result = data * juridical.percents / 100;
-  if (result < juridical.min.amount) {
-    return(juridical.min.amount);
-  }
-  else return(result);
-}
-
-function getDifferentId(data) {
-  //gets all distinct id's 
-  let limit =  [
-    ...new Set(
-      data.map(({ user_id, user_type }) => {
-        if (user_type == "natural") return user_id;
-        else return null;
-      })
-    ),
-  ];
-  //returns id's without null values
-  return limit.filter((n) => n);
 }
 
 function SameWeek(data, i, limit) {
   const currentDate = data[i].date;
-
-  //Getting last cashout date with same user Id
+  const weekLimit = limit;
+  // Getting last cashout date with same user Id
   const lastCashOutDate = GetLastOperationDate(data, data[i].user_type, i);
 
-  //get days difference between last cashout and current
+  // get days difference between last cashout and current
   const days = (new Date(currentDate) - new Date(lastCashOutDate)) / 1000 / 60 / 60 / 24;
   const current = new Date(currentDate).getDay();
   const last = new Date(lastCashOutDate).getDay();
 
-  //checking if it's same week
+  // checking if it's same week
   if (days < 7) {
-    if ((current >= last || current == 0) && last != 0) {
-      return limit;
-    } else if (last == 0 && current == 0) return limit;
-    else {
-      limit[data[i].user_id] = 0;
-      return limit;
+    if ((current >= last || current === 0) && last !== 0) {
+      return weekLimit;
+    } if (last === 0 && current === 0) return weekLimit;
+
+    weekLimit[data[i].user_id] = 0;
+    return weekLimit;
+  }
+  weekLimit[data[i].user_id] = 0;
+  return weekLimit;
+}
+
+function CountPercents(amount, percents) {
+  return (amount * percents) / 100;
+}
+
+function Natural(data, i, natural, idsLimit) {
+  const idLimit = SameWeek(data, i, idsLimit);
+  idLimit[data[i].user_id] += data[i].operation.amount;
+  const amount = idLimit[data[i].user_id];
+
+  if (amount <= natural.week_limit) {
+    return 0;
+  } if (amount > natural.week_limit.amount) {
+    if (data[i].operation.amount === amount) {
+      return CountPercents(
+        data[i].operation.amount - natural.week_limit.amount,
+        natural.percents,
+      );
     }
+    if (amount - data[i].operation.amount < natural.week_limit.amount) {
+      return CountPercents(
+        amount - natural.week_limit.amount,
+        natural.percents,
+      );
+    } return CountPercents(data[i].operation.amount, natural.percents);
   }
-  else {
-    limit[data[i].user_id] = 0;
-    return limit;
-  }
+  return 0;
 }
 
-function GetLastOperationDate(data, id, i) {
-  for (let index = i - 1; index >= 0; index--) {
-    if (data[index].user_type == id && data[index].type == "cash_out") {
-      return data[index].date;
-    } else if (index == 0) return data[i].date;
-  }
+function Juridical(data, juridical) {
+  const result = CountPercents(data, juridical.percents);
+  if (result < juridical.min.amount) {
+    return juridical.min.amount;
+  } return result;
 }
 
-function PrintAnswer(answer)
-{
-  answer = answer.toString();
-  countAfterComma = (answer % 1).toString().length;
-  if (countAfterComma >= 5) {
-    console.log((+parseFloat(answer).toFixed(2) + 0.01).toFixed(2));
-  }
-  else {
-    console.log(parseFloat(answer).toFixed(2));
-  }
+function GetDifferentId(data) {
+  // gets all distinct id's
+  const limit = [
+    ...new Set(
+      data.map(({ user_id, user_type }) => {
+        if (user_type === 'natural') return user_id;
+        return null;
+      }),
+    ),
+  ];
+  // returns id's without null values
+  return limit.filter((n) => n);
 }
+
+function PrintAnswer(answer) {
+  const data = answer * 100;
+  const percents = Math.ceil(data);
+  console.log((percents / 100).toFixed(2));
+}
+
+fs.readFile(
+  process.argv[2] ? process.argv[2] : 'input.json',
+  (async (err, json) => {
+    // Fetching operation values from Api's
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    const data = JSON.parse(json);
+
+    const cashIn = await FetchData('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-in');
+    const natural = await FetchData('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/natural');
+    const juridical = await FetchData('http://private-38e18c-uzduotis.apiary-mock.com/config/cash-out/juridical');
+
+    // Getting different id's from given data to count natural limits for each natural user
+    let idsLimit = [];
+    idsLimit = GetDifferentId(data);
+
+    const limit = {};
+
+    // making initial used limit for each user 0
+    idsLimit.map((el) => {
+      limit[el] = 0;
+      return limit[el];
+    });
+
+    // Making operations
+    let answer = '';
+    data.forEach((el, i) => {
+      if (el.type === 'cash_in') {
+        answer = CashIn(el, cashIn);
+      } else if (el.type === 'cash_out') {
+        if (el.user_type === 'natural') {
+          const limits = Natural(data, i, natural, limit);
+          answer = limits;
+        } else if (el.user_type === 'juridical') {
+          answer = Juridical(el.operation.amount, juridical);
+        }
+      }
+      PrintAnswer(answer);
+    });
+  }),
+);
 
 module.exports = {
-  fetchData: fetchData,
-  CashIn: CashIn,
-  Natural: Natural,
-  Juridical: Juridical,
-  getDifferentId: getDifferentId,
-  SameWeek: SameWeek,
-  GetLastOperationDate: GetLastOperationDate
-}
-
+  FetchData,
+  CashIn,
+  Natural,
+  Juridical,
+  GetDifferentId,
+  SameWeek,
+  GetLastOperationDate,
+};
--- Js/test/appTest.js
+++ Js/test/appTest.js
@@ -1,73 +1,76 @@
-const assert = require("chai").assert;
-const app = require("../app");
+/* eslint-disable no-shadow */
+/* eslint-disable no-undef */
+const { assert } = require('chai');
+const app = require('../app');
 const data = require('./testinput');
 const cashIn = require('./cashIn');
 const juridical = require('./Juridical');
 const natural = require('./Natural');
 
-describe("App", function () {
-  describe("fetchData", function () {
-    it("fetchData should return json object", async function () {
-      const result = await app.fetchData(
-        "http://private-38e18c-uzduotis.apiary-mock.com/config/cash-in"
-      );
-      assert.typeOf(result, "object");
+describe('App', () => {
+  describe('GetDifferentId', () => {
+    it('GetDifferentId should return array', () => {
+      const result = app.GetDifferentId(data);
+      assert.typeOf(result, 'array');
     });
   });
-  describe("getDifferentId", function () {
-    it("getDifferentId should return array", function () {
-      const result = app.getDifferentId(data);
-      assert.typeOf(result, "array");
-    });
-  });
-  describe("SameWeek", function () {
-    it("SameWeek should return object", function () {
-      const limit = [{ "1": 0, "3": 0 }];
+  describe('SameWeek', () => {
+    it('SameWeek should return object', () => {
+      const limit = [{ 1: 0, 3: 0 }];
       const result = app.SameWeek(data, 2, limit);
-      assert.typeOf(result, "array");
+      assert.typeOf(result, 'array');
     });
   });
-  describe("Juridical", function () {
-    it("Juridical should return number more than Juridical minimum amount", function () {
-      const result = app.Juridical(300, juridical);
+  describe('Juridical', () => {
+    const result = app.Juridical(300, juridical);
+    it('Juridical should return number more than Juridical minimum amount', () => {
       assert.isAbove(result, juridical.min.amount);
     });
-  });
-  describe("CashIn", function () {
-    it("CashIn should return fee less or equal to max fee amount ", function () {
-      const data = {
-        date: "2016-01-10",
-        user_id: 2,
-        user_type: "juridical",
-        type: "cash_in",
-        operation: { amount: 1000000, currency: "EUR" },
-      };
-      const result = app.CashIn(data, cashIn);
-      assert.isAtMost(result, cashIn.max.amount);
+    it('Juridical should return number', () => {
+      assert.typeOf(result, 'number');
     });
   });
-  describe("Natural", function () {
-    it("Natural should return fee if limit is greater than weekLimit", function () {
-      const data = [
-        {
-          date: "2016-01-06",
-          user_id: 1,
-          user_type: "natural",
-          type: "cash_out",
-          operation: { amount: 30000, currency: "EUR" },
-        },
-        {
-          date: "2016-01-07",
-          user_id: 1,
-          user_type: "natural",
-          type: "cash_out",
-          operation: { amount: 1000.0, currency: "EUR" },
-        },
-        ];
-        let limit = { '1': 0, '3': 0 };
-        const result = app.Natural(data, 1, natural, limit);
-        if (limit > natural.week_limit.amount) assert.isAbove(result, 0, 'limit is exceeded');
-        else assert.equal(result, 0, 'limit is not exceeded');
+  describe('CashIn', () => {
+    const data = {
+      date: '2016-01-10',
+      user_id: 2,
+      user_type: 'juridical',
+      type: 'cash_in',
+      operation: { amount: 1000000, currency: 'EUR' },
+    };
+    const result = app.CashIn(data, cashIn);
+    it('CashIn should return fee less or equal to max fee amount ', () => {
+      assert.isAtMost(result, cashIn.max.amount);
+    });
+    it('CashIn should return number', () => {
+      assert.typeOf(result, 'number');
+    });
+  });
+  describe('Natural', () => {
+    const data = [
+      {
+        date: '2016-01-06',
+        user_id: 1,
+        user_type: 'natural',
+        type: 'cash_out',
+        operation: { amount: 30000, currency: 'EUR' },
+      },
+      {
+        date: '2016-01-07',
+        user_id: 1,
+        user_type: 'natural',
+        type: 'cash_out',
+        operation: { amount: 1000.0, currency: 'EUR' },
+      },
+    ];
+    const limit = { 1: 0, 3: 0 };
+    const result = app.Natural(data, 1, natural, limit);
+    it('Natural should return fee if limit is greater than weekLimit', () => {
+      if (limit > natural.week_limit.amount) assert.isAbove(result, 0, 'limit is exceeded');
+      else assert.equal(result, 0, 'limit is not exceeded');
+    });
+    it('Natural should return number', () => {
+      assert.typeOf(result, 'number');
     });
   });
 });
